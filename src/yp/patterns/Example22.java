7d17354d0a3cc29f613bac598d04ad0e504bc68d
Yogesh <Yogesh>
2025-08-12 16:41:09 +0530

    pattens half

---
+++ src/yp/patterns/Example22.java
@@ -0,0 +1,29 @@
+package yp.patterns;
+
+public class Example22 {
+    public static void main(String[] args) {
+        int n=6;
+        for(int i=0;i<2*n;i++){
+            int s;
+            int g;
+            if(i>=n){
+                g=2*(2*n-i)-2;
+                s=i+1-n;
+            }else{
+                s=n-(i);
+                g=2*i;
+            }
+
+            for(int j=0;j<s;j++){
+                System.out.print("* ");
+            }
+            for (int k=0;k<g;k++){
+                System.out.print("  ");
+            }
+            for(int j=0;j<s;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ src/yp/patterns/Example24.java
@@ -0,0 +1,33 @@
+package yp.patterns;
+
+public class Example24 {
+    public static void main(String[] args) {
+        int n=3;
+        for(int i=1;i<=n;i++) {
+            if (i == 1 || i == n) {
+
+                for(int j=1;j<=n;j++){
+                    System.out.print("* ");
+                }
+                System.out.println();
+            } else {
+
+                for (int j = 1; j < 2; j++) {
+                    System.out.print("* ");
+                }
+                for (int j = 1; j <= n - 2; j++) {
+                    System.out.print("  ");
+                }
+                for (int j = 1; j < 2; j++) {
+                    System.out.print("* ");
+                }
+                System.out.println();
+            }
+        }
+    }
+}
+
+
+/*
+
+*/
